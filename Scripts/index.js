ea63b525da880407b78937359e87384fafb37c1c
biancavee15 <vasquezbmarie@gmail.com>
2018-03-01 14:46:56 -0500

    added some basic html

---
+++ Scripts/index.js
@@ -0,0 +1,1 @@
+//use this place to test
---
+++ Scripts/store.js
@@ -0,0 +1,19 @@
+const store = {
+    bookmarks = [{
+        id: 1,
+        title: 'Lean Analytics',
+        rating: 3,
+        expaned: false,
+    }, {
+        id: 2,
+        title: 'MDN Documents',
+        rating: 5,
+        expaned: false,
+    }, {
+        id: 3,
+        title: 'Thinkful-Dashboard',
+        rating: 2,
+        expanded: false,
+    }],
+    adding: true,
+};
